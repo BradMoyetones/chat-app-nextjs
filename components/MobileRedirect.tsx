92eaa957855e92709235cbb9cbdab34e86a303ac
BradMoyetones <brad.moyetones@gmail.com>
2025-06-01 00:36:32 -0500

    Mobile redirect native

--- components/MobileRedirect.tsx
+++ components/MobileRedirect.tsx
@@ -3,21 +3,19 @@
 import { useEffect } from 'react'
 import { usePathname, useRouter } from 'next/navigation'
 import { useIsTrulyMobile } from '@/hooks/useIsTrulyMobile'
+import { useAuth } from '@/contexts/AuthContext'
 
 export function MobileRedirect() {
     const isMobile = useIsTrulyMobile()
     const pathname = usePathname()
     const router = useRouter()
+    const {user} = useAuth()
 
     useEffect(() => {
-        if (!isMobile) return
-
-        const isAlreadyInMb = pathname.startsWith('/mb')
-
-        if (!isAlreadyInMb) {
-            router.replace('/mb/chats')
-        }
-    }, [isMobile, pathname, router])
+        if(!user) return
+        if (!isMobile) return router.replace('/chats')
+        if (isMobile) return router.replace('/mb/chats')
+    }, [isMobile, pathname, router, user])
 
     return null
 }
